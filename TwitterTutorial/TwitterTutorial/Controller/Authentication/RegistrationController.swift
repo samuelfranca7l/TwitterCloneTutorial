3ec87123b2a0b2950400026bd2ac40a5c4c30c34
Samuel <samuel@MacBook-Pro-de-Samuel.local>
2022-11-29 18:13:42 -0300

    Adicionando funcionalidades a tela de cadastro

--- TwitterTutorial/TwitterTutorial/Controller/Authentication/RegistrationController.swift
+++ TwitterTutorial/TwitterTutorial/Controller/Authentication/RegistrationController.swift
@@ -11,6 +11,69 @@ class RegistrationController: UIViewController {
     
     // MARK: - Properties
     
+    private let imagePicker = UIImagePickerController()
+    
+    private let plusPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "plus_photo"), for: .normal)
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(handleAddProfilePhoto), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var emailContainerView: UIView = {
+        let view = Utilities().inputContainerView(image: "outline_mail_outline_white_36pt_1x", textField: emailTextField)
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let view = Utilities().inputContainerView(image: "ic_lock_outline_white_2x", textField: passwordTextField)
+        return view
+    }()
+    
+    private lazy var fullNameContainerView: UIView = {
+        let view = Utilities().inputContainerView(image: "outline_mail_outline_white_36pt_1x", textField: fullNameTextField)
+        return view
+    }()
+    
+    private lazy var usernameContainerView: UIView = {
+        let view = Utilities().inputContainerView(image: "ic_lock_outline_white_2x", textField: usernameTextField)
+        return view
+    }()
+    
+    private lazy var emailTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Email")
+        return tf
+    }()
+    
+    private lazy var passwordTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Password")
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    
+    private lazy var fullNameTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Full Name")
+        return tf
+    }()
+    
+    private lazy var usernameTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Username")
+        return tf
+    }()
+    
+    private let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.setTitleColor(.twitterBlue, for: .normal)
+        button.backgroundColor = .white
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        return button
+    }()
+    
     private let alreadyHaveAccountButton: UIButton = {
         let button = Utilities().attributedButton(firstPart: "Already have an account?", secondPart: " Sign In")
         button.addTarget(self, action: #selector(handleShowLogin), for: .touchUpInside)
@@ -24,21 +87,71 @@ class RegistrationController: UIViewController {
         configureUI()
     }
     
-    // MARK: - Selectors
+    // MARK: - Selectors®
     
     @objc func handleShowLogin() {
         navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func handleSignUp() {
+        print("Sign Up...")
+    }
+    
+    @objc func handleAddProfilePhoto() {
+        present(imagePicker, animated: true, completion: nil)
     }
     
     // MARK: - Helpers
     
     func configureUI() {
         view.backgroundColor = .twitterBlue
+        navigationController?.navigationBar.barStyle = .black //Faz tudo que está dentro da navigation bar ficar BRANCO por exemplo o relógio, a bateria, o sinal do wifi, etc.
         
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        
+        view.addSubview(plusPhotoButton)
+        plusPhotoButton.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
+        plusPhotoButton.setDimensions(width: 128, height: 128)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView,
+                                                   passwordContainerView,
+                                                   fullNameContainerView,
+                                                   usernameContainerView ,
+                                                   signUpButton])
+        stack.axis = .vertical
+        stack.spacing = 20
+        stack.distribution = .fillEqually
+        
+        view.addSubview(stack)
+        stack.anchor(top: plusPhotoButton.bottomAnchor, left: view.leftAnchor,
+                     right: view.rightAnchor, paddingTop: 32,
+                     paddingLeft: 32, paddingRight: 32)
+                
         view.addSubview(alreadyHaveAccountButton)
         alreadyHaveAccountButton.anchor(left: view.leftAnchor,
                                      bottom: view.safeAreaLayoutGuide.bottomAnchor,
                                      right: view.rightAnchor, paddingLeft: 40,
                                      paddingRight: 40)
+    }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+
+extension RegistrationController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let profileImage = info[.editedImage] as? UIImage else { return }
+        
+        plusPhotoButton.layer.cornerRadius = 128 / 2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.imageView?.contentMode = .scaleAspectFill
+        plusPhotoButton.imageView?.clipsToBounds = true
+        plusPhotoButton.layer.borderColor = UIColor.white.cgColor
+        plusPhotoButton.layer.borderWidth = 3
+        
+        self.plusPhotoButton.setImage(profileImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        
+        dismiss(animated: true, completion: nil)
     }
 }
